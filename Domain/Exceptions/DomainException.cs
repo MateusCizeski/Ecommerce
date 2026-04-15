2189895712b4a63310c2ce4c9fc4e872768f42f9
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-04-14 21:24:39 -0300

    initial structure of ecommerce

---
+++ Domain/Exceptions/DomainException.cs
@@ -0,0 +1,18 @@
+﻿namespace Ecommerce.Domain.Exceptions
+{
+    public class DomainException : Exception
+    {
+        public DomainException(string message) : base(message) { }
+        public DomainException(string message, Exception inner) : base(message, inner) { }
+    }
+
+    public class NotFoundException : DomainException
+    {
+        public NotFoundException(string entity, object key) : base($"Entity '{entity}' with key '{key}' was not found.") { }
+    }
+
+    public class TenantAccessException : DomainException
+    {
+        public TenantAccessException() : base("Access denied: resource does not belong to the current tenant.") { }
+    }
+}
