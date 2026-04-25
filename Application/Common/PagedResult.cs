14a06b160feeca7af17f797b15dacff568229cf6
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-04-24 23:56:23 -0300

    page result

---
+++ Application/Common/PagedResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Common;
+
+public record PagedResult<T>(IEnumerable<T> Items, int Total, int Page, int PageSize)
+{
+    public int TotalPages => (int)Math.Ceiling((double)Total / PageSize);
+    public bool HasNextPage => Page < TotalPages;
+    public bool HasPreviousPage => Page > 1;
+}
