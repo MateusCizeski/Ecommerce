0bb4f5397498b2fa3785ea66e2320014fb94eac4
Mateus Cizeski Kogl <mateuscizeski@gmail.com>
2026-04-25 11:57:09 -0300

    Interfaces, exceptions application

---
+++ Application/Exceptions/LoggingBehavior.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Exceptions;
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
+    {
+        var name = typeof(TRequest).Name;
+        logger.LogInformation("Handling {RequestName}", name);
+        try
+        {
+            var response = await next();
+            logger.LogInformation("Handled {RequestName} successfully", name);
+            return response;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Error handling {RequestName}", name);
+            throw;
+        }
+    }
+}
